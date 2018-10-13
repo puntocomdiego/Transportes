c17889e1878c6c0e45023d3ca2612982810ca51f
puntocomdiego <44074906+puntocomdiego@users.noreply.github.com>
2018-10-13 00:22:34 -0300

    asd

--- Transportes/Form_Prueba.Designer.vb
+++ Transportes/Form_Prueba.Designer.vb
@@ -22,8 +22,28 @@ Partial Class Form_Prueba
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(86, 61)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(127, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form_Prueba
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form_Prueba"
         Me.Text = "Form_Prueba"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
